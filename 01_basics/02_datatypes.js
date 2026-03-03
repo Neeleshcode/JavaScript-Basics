5ff4bf92228a02abdda1027eed00f281fec85223
Neelesh Kumar <121389298+Neeleshcode@users.noreply.github.com>
2026-03-03 04:07:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all code as newer version
+
+// alert( 3+3); // we are using node.js not browser 
+
+console.log( 3
+    + 
+    3)  // code readibility should be high
+
+console.log("Neelesh")
+
+let name = "Neelesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+let city;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+//object 
+
+
+console.log(typeof "Neelesh");
+console.log(typeof age);
+console.log(typeof null); //null
+console.log(typeof undefined); //undefined
+
