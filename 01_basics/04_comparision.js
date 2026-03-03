f860aee92d55bf55c022e11d0e77a79a867d637b
Neelesh Kumar <121389298+Neeleshcode@users.noreply.github.com>
2026-03-03 13:18:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+/* Note: Avoid such type of conversions as it is confusing */
+
+// === --> checks the datatype comparision
+
+console.log("2" === 2);
