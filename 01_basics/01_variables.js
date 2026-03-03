7e2185513518b16cbef1cf5ca3eacf8c0f1251ca
Neelesh Kumar <121389298+Neeleshcode@users.noreply.github.com>
2026-03-03 03:38:06 +0000

    Learnt variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 144553
+let accountEmail = "yadavneelesh669@gmail.com"
+var accountPassword = "12334"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 2 --> Not allowed //
+
+accountEmail = "yadavneelesh923@gmail.com"
+accountPassword ="226008"
+accountCity = "Delhi"
+
+console.log(accountID)
+
+/* Prefer not to use var
+Because of issues faced in block scope and funtional scope */
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
