79663eae8deb0a3d60b80c29113fc6d461095ad4
Neelesh Kumar <121389298+Neeleshcode@users.noreply.github.com>
2026-03-03 05:23:24 +0000

    conversions and operations

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -14,8 +14,8 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Neelesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof(booleanIsLoggedIn))
-console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +24,55 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+//************* Operations ****************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Neelesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1 + 2 +"2");
+
+console.log((3 +4) * 5 % 3); 
+//--> try to write a neat code
+
+console.log(+true);
+// console.log(true+);
+// -->provides error not possible
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+// mdn docs js --> https://developer.mozilla.org/en-US/docs/Web/JavaScript
+// link --> https://tc39.es/ecma262/2023/multipage/
+
+
+
+
